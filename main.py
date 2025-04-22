4cf5e601eeb075b4fd4957ae306ac245ed937514
mmahnoosh <montazer_mahnoosh@gmx.de>
2025-04-22 16:53:35 +0200

    first commit fix whatsapp_message     detect_language     send_translated_message

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import translataion_api
 import Twilio
 import client
+from langdetect import detect
 
 
-def should_be_message_translate(lang_message = "DE"):
-    #return lang_message != profile_lang()
+def should_be_message_translate(receiver_lang = "DE"):
+    #return receiver_lang != profile_lang()
     pass
 
+def ask_sender_for_translation_choice():
+    return input("Would you like to translate? (y/n)").lower()
+
+
 def whatsapp_message():
+
     user_message = funktion_api_Gent()
     if should_be_message_translate():
         funktion_api_Gerd()
@@ -21,13 +27,30 @@ def funktion_api_Gent():
     return "translate"
 
 
+def detect_language(text):
+    try:
+        return detect(text)
+    except:
+        return "unknown"
+
+
+def send_translated_message():
+     pass
+
 def dummy():
     pass
 
 
 def main():
     dummy()
-
+    msg = whatsapp_message()
+    receiver_lang = detect_language(msg)
+    choice = ask_sender_for_translation_choice()
+    if choice == "y":
+        translated = funktion_api_Gerd()
+        send_translated_message()
+    else:
+        send_translated_message()
 
 if  __name__ == '__main__':
     main()
