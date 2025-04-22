25fa5138870500111adc701642354f95c924a734
mmahnoosh <montazer_mahnoosh@gmx.de>
2025-04-22 15:55:11 +0200

    first commit fix should_be_message_translate() and whatsapp_message()

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+import translataion_api
+import Twilio
+import client
+
+
+def should_be_message_translate(lang_message = "DE"):
+    #return lang_message != profile_lang()
+    pass
+
+def whatsapp_message():
+    user_message = funktion_api_Gent()
+    if should_be_message_translate():
+        funktion_api_Gerd()
+    return user_message
+
+def funktion_api_Gerd():
+    pass
+
+
+def funktion_api_Gent():
+    return "translate"
+
+
 def dummy():
     pass
 
@@ -6,5 +29,5 @@ def main():
     dummy()
 
 
-if __name__ == '__main__':
+if  __name__ == '__main__':
     main()
