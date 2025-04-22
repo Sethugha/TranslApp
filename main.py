615f0aec1ad3024ee0891a5811948d862ce16ac9
mmahnoosh <montazer_mahnoosh@gmx.de>
2025-04-22 21:51:57 +0200

    Fix docstring.

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ import client"""
 
 import translate_functions
 
+
 def dummy():
     pass
 
 
 def main():
     dummy()
-
+    print("The text language is :",
+          translate_functions.detect_language(translate_functions.funktion_api_Gerd()))
     print(translate_functions.whatsapp_message())
 
-if  __name__ == '__main__':
+
+if __name__ == '__main__':
     main()
