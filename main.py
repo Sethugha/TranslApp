c94daa40e78d1336852f470ee6ef5b36d7cc1cc0
mmahnoosh <montazer_mahnoosh@gmx.de>
2025-04-22 21:20:04 +0200

    Improve functionality.
    - fix ask_sender_for_translation_choice()
    - delete unneeded function
    - fix whatsapp_message
    - Add translate_functions.py

--- main.py
+++ main.py
@@ -1,41 +1,8 @@
-import translataion_api
+"""import translataion_api
 import Twilio
-import client
-from langdetect import detect
+import client"""
 
-
-def should_be_message_translate(receiver_lang = "DE"):
-    #return receiver_lang != profile_lang()
-    pass
-
-def ask_sender_for_translation_choice():
-    return input("Would you like to translate? (y/n)").lower()
-
-
-def whatsapp_message():
-
-    user_message = funktion_api_Gent()
-    if should_be_message_translate():
-        funktion_api_Gerd()
-    return user_message
-
-def funktion_api_Gerd():
-    pass
-
-
-def funktion_api_Gent():
-    return "translate"
-
-
-def detect_language(text):
-    try:
-        return detect(text)
-    except:
-        return "unknown"
-
-
-def send_translated_message():
-     pass
+import translate_functions
 
 def dummy():
     pass
@@ -43,14 +10,14 @@ def dummy():
 
 def main():
     dummy()
-    msg = whatsapp_message()
-    receiver_lang = detect_language(msg)
-    choice = ask_sender_for_translation_choice()
+    msg = translate_functions.whatsapp_message()
+    receiver_lang = translate_functions.detect_language(msg)
+    choice = translate_functions.ask_sender_for_translation_choice()
     if choice == "y":
-        translated = funktion_api_Gerd()
-        send_translated_message()
+        translated = translate_functions.funktion_api_Gerd()
+        translate_functions.send_translated_message()
     else:
-        send_translated_message()
+        translate_functions.send_translated_message()
 
 if  __name__ == '__main__':
     main()
---
+++ translate_functions.py
@@ -0,0 +1,33 @@
+from langdetect import detect
+def ask_sender_for_translation_choice():
+    while True:
+        choice = input("Would you like to translate? (y/n)").lower()
+        if choice == 'y':
+            return True
+        elif choice == 'n':
+            return False
+
+
+def whatsapp_message():
+    user_message = funktion_api_Gent()
+    if ask_sender_for_translation_choice():
+        funktion_api_Gerd()
+    return user_message
+
+def funktion_api_Gerd():
+    pass
+
+
+def funktion_api_Gent():
+    return "translate"
+
+
+def detect_language(text):
+    try:
+        return detect(text)
+    except:
+        return "unknown"
+
+
+def send_translated_message():
+     pass
